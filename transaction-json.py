5b127aadcaba484a6e98306acc9da5f7e3c72d76
Dean Wilson <dean.wilson@gmail.com>
2011-01-10 19:39:13 +0000

    first commit

---
+++ transaction-json.py
@@ -0,0 +1,32 @@
+from yum.plugins import PluginYumExit, TYPE_CORE, TYPE_INTERACTIVE
+import simplejson
+
+requires_api_version = '2.5'
+
+plugin_type = (TYPE_INTERACTIVE,)
+
+def config_hook(conduit):
+    parser = conduit.getOptParser()
+    parser.add_option('', '--json', dest='json', action='store_true',
+           default=False, help="show pending package changes as JSON")
+
+def postresolve_hook(conduit):
+    opts, commands = conduit.getCmdLine()
+
+    if opts.json:
+        packages = {}
+
+        for transaction in conduit.getTsInfo():
+            if transaction.name not in packages:
+                packages[transaction.name] = {}
+
+            version = { "version": transaction.version, "release": transaction.release,
+                    "epoch": transaction.epoch, "arch": transaction.arch, "state": transaction.ts_state }
+
+            if transaction.ts_state:
+                packages[transaction.name]["pending"] = version
+            else:
+                packages[transaction.name]["current"] = version
+
+        print( simplejson.dumps(packages) )
+        raise PluginYumExit('')
